05a15471e8cc29aee1339355be900b6e6d2b087c
william jose anez <WILLIAMJOSEANEZ@GMAIL.COM>
2023-09-21 23:03:46 -0300

    agregando libreria

---
+++ swa/sweetaler.js
@@ -0,0 +1,9 @@
+Swal.fire({
+    title: 'Bienvenido a mi proyecto JS',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  })
