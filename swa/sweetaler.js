158f85df75e89b4e5a2b838f1995dd03bd2e0a26
william jose anez <WILLIAMJOSEANEZ@GMAIL.COM>
2023-09-22 00:19:23 -0300

    agregando modific

--- swa/sweetaler.js
+++ swa/sweetaler.js
@@ -1,5 +1,7 @@
 Swal.fire({
-    title: 'Bienvenido a mi proyecto JS',
+  title: 'Bienvenido a mi proyecto JS',
+  confirmButtonText: 'CONTINUAR',
+  footer: 'Gracias por entrar a mi pagina web',
     showClass: {
       popup: 'animate__animated animate__fadeInDown'
     },
